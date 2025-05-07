b3794cf3731b65eb411cc4a5a5b61eca4f9ca129
arslanmatloob <arslan@earnflex.com>
2025-05-07 22:39:12 +0500

    font-problem

--- dist/assets/supplier-8d74d849.js
+++
@@ -1,1 +0,0 @@
-import{f as s,B as e,C as t,O as a}from"./index-7009f34f.js";const r=s({__name:"supplier",setup:s=>(s,r)=>{const o=e("RouterView");return t(),a(o)}});export{r as default};
---
+++ dist/assets/supplier-f98d495a.js
@@ -0,0 +1,1 @@
+import{f as s,B as e,C as t,O as a}from"./index-ff8cc036.js";const r=s({__name:"supplier",setup:s=>(s,r)=>{const o=e("RouterView");return t(),a(o)}});export{r as default};
